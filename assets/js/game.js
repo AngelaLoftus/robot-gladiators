01a27943a803c424a3d009a363a5e43bebbb3912
Angela Loftus <angelaloftus@yahoo.com>
2021-08-15 23:31:43 -0500

    add arrays

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,11 +14,11 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function (enemyName){
-    //alert players that the round has started
-    window.alert("Welcome to Robot Gladiators!");
+
+    //repeat and execute as long as the enemhy-robot is alive
+    while (enemyHealth > 0){
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
 
     //if player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT"){
@@ -72,9 +72,12 @@ else {
     window.alert("You need to choose a valid option. Try again!");
 }
 
+}
 }; 
 
 
 for (var i = 0; i < enemyNames.length; i++){
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth=50;
+    fight(pickedEnemyName);
 }
